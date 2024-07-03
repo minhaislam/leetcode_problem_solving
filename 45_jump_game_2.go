2bb4228c3e8149dcb37aa84968e94b0c6cad7920
minhaislam <minhajislam95@gmail.com>
2024-07-04 00:38:44 +0600

    Solved 45_jump_game_2 problem

--- 45_jump_game_2.go
+++ 45_jump_game_2.go
@@ -16,7 +16,7 @@ func main() {
 	start := time.Now()
 	// Code to measure
 
-	nums := []int{2, 3, 1, 1, 4}
+	nums := []int{2, 3, 0, 1, 4}
 
 	// output := []int{1, 2, 2, 3, 5, 6}
 
@@ -27,27 +27,30 @@ func main() {
 }
 
 func jump(nums []int) int {
-	i := len(nums) - 2
-	target_index := len(nums) - 1
+	i := 0
+	// initial_value := nums[0]
+	// // step := initial_value
+	current_index := 0
 	count := 0
-	// target_index := len(nums) - 1
-	// if len
-	// dead_end := 0
-	// dead_end_index := 0
-	for i >= 0 {
-		if nums[i]+i >= target_index && nums[i] > 0 {
-			fmt.Println(target_index, "---", nums[i], " index ", i)
-			target_index = i
-			count++
-			fmt.Println(count)
-		} else {
-			fmt.Println(target_index, "---", nums[i], " index ", i)
-			count--
-			fmt.Println(count)
-
+	max_val_index := 0
+	for i < len(nums)-1 {
+		fmt.Println("value: ", nums[i])
+		if max_val_index < i+nums[i] {
+			max_val_index = i + nums[i]
 		}
-		i--
+
+		if max_val_index >= len(nums)-1 {
+			count++
+			break
+		}
+		if i == current_index {
+			count++
+			current_index = max_val_index
+		}
+
+		i++
 	}
+	// fmt.Println("current_index: ", current_index)
 	fmt.Println(count)
 	return count
 }
